4ed24d94a9ec72fd87057ff05816527e4b087523
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-11-12 18:53:05 +0300

    realized drowing rect

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import '../styles/toolBar.scss'
+import '../styles/toolBar.scss';
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
+import Rect from '../tools/Rect';
 
 const ToolBar = () => {
   return (
     <div className='toolBar'>
-      <button className='toolBar__btn brush'/>
-      <button className='toolBar__btn rect'/>
+      <button className='toolBar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
+      <button className='toolBar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))}/>
       <button className='toolBar__btn circle'/>
       <button className='toolBar__btn eraser'/>
       <button className='toolBar__btn line'/>
