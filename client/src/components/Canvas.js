59fee879b309187107475f80dd75e90128fa7de7
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-11-12 16:48:24 +0300

    realized brush painting

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/canvas.scss';
+import {observer} from 'mobx-react-lite';
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
 
-const Canvas = () => {
+const Canvas = observer(() => {
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current)
+    toolState.setTool(new Brush(canvasRef.current))
+  }, [])
+
   return (
     <div className='canvas'>
-      <canvas width={700} height={500}/>
+      <canvas ref={canvasRef} width={700} height={500}/>
     </div>
   );
-}
+})
 
 export default Canvas;
