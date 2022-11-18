75f91210d6cabe3c22efc5b9b97d3011b058d424
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-11-18 22:34:14 +0300

    realized webSocket server, connection to session

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,36 @@ import React from 'react';
 import SettingBar from './components/SettingBar';
 import ToolBar from './components/ToolBar';
 import Canvas from './components/Canvas';
-import './styles/app.scss'
+import './styles/app.scss';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app'>
-      <ToolBar/>
-      <SettingBar/>
-      <Canvas/>
+    <BrowserRouter>
+      <div className='app'>
+      <Routes>
+        <Route path='/:id' element={
+          <>
+            <ToolBar/>
+            <SettingBar/>
+            <Canvas/>
+          </>} 
+        />
+        <Route path='/' element={
+          <>
+            <ToolBar/>
+            <SettingBar/>
+            <Canvas/>
+            <Navigate to={`/f${(+new Date()).toString(16)}`} replace/>
+          </>} 
+        />
+      </Routes>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
 export default App;
+
+
--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -1,24 +1,70 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../styles/canvas.scss';
 import {observer} from 'mobx-react-lite';
 import canvasState from '../store/canvasState';
 import toolState from '../store/toolState';
 import Brush from '../tools/Brush';
+import { Modal, Button } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
 
 const Canvas = observer(() => {
   const canvasRef = useRef()
+  const userNameRef = useRef()
+  const [modal, setModal] = useState(true)
+  const params = useParams()
+  console.log(params)
 
   useEffect(() => {
     canvasState.setCanvas(canvasRef.current)
     toolState.setTool(new Brush(canvasRef.current))
   }, [])
 
+  useEffect(() => {
+    if (canvasState.userName) {
+      const socket = new WebSocket('ws://localhost:5000/')
+      socket.onopen = () => {
+        console.log('Connection OK')
+        socket.send(JSON.stringify({
+          id: params.id,
+          userName: canvasState.userName,
+          method:'connection'
+        }))
+      }
+      socket.onmessage = (event) => {
+        console.log(event.data)
+      }
+    }
+    
+  }, [canvasState.userName])
+
   const mouseDownHandler = () => {
     canvasState.pushToUndo(canvasRef.current.toDataURL())
   }
 
+  const connectionHandler = () => {
+    canvasState.setUserName(userNameRef.current.value)
+    setModal(false)
+  }
+
+  const hide = () => {
+
+  }
+
   return (
     <div className='canvas'>
+      <Modal show={modal} onHide={hide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Enter your name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type='text' ref={userNameRef}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={connectionHandler}>
+            Sign In
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <canvas onMouseDown={() => mouseDownHandler()} ref={canvasRef} width={700} height={500}/>
     </div>
   );
